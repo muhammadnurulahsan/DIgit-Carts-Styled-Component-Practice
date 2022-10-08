cadf53f1e3ab2deb21d3e2d4d1ca45bf6e30bee1
muhammadnurulahsan <mdnurulahsanbdbd@gmail.com>
2022-10-08 15:39:57 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
+import "./styles.css";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+import StyledButton, {
+  FancyButton,
+  SubmitButton,
+  AnimatedLogo,
+  DarkButton,
+} from "./Components/Button/Button";
+
+const theme = {
+  dark: {
+    primary: "#000",
+    text: "#fff",
+  },
+  light: {
+    primary: "#fff",
+    text: "#000",
+  },
+  fontFamily: "Segoe UI",
+};
+
+const GlobalStyle = createGlobalStyle`
+button{
+  font-family: ${(props) => props.theme.fontFamily};
+}
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <div className="App">
+        {/* <img src={logo} alt="logo" className="App-logo" /> */}
+        <AnimatedLogo src={logo} />
+        <div>
+          <br />
+        </div>
+        <StyledButton type="submit">Styled Button</StyledButton>
+        <div>
+          <br />
+        </div>
+        <StyledButton variant="outline">Styled Button</StyledButton>
+        <div>
+          <br />
+        </div>
+        <FancyButton as="a">Styled Button</FancyButton>
+        <div>
+          <br />
+        </div>
+        <SubmitButton>Submit Button</SubmitButton>
+        <div>
+          <br />
+        </div>
+        <DarkButton>Dark Button</DarkButton>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/Components/Button/Button.js
@@ -0,0 +1,9 @@
+import {
+  StyledButton,
+  FancyButton,
+  SubmitButton,
+  AnimatedLogo,
+  DarkButton,
+} from "./Button.styles";
+export default StyledButton;
+export { FancyButton, SubmitButton, AnimatedLogo, DarkButton };
